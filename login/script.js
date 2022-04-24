6e6841b4e9a57301841c40dc13588bb578a50a08
Sri Vamsi Rajesh <83352740+SrivamsiRajesh@users.noreply.github.com>
2022-04-24 20:03:18 +0400

    Add files via upload

---
+++ login/script.js
@@ -0,0 +1,18 @@
+function switchForm(className, e) {
+	e.preventDefault();
+	const allForm = document.querySelectorAll('form');
+	const form = document.querySelector(`form.${className}`);
+
+	allForm.forEach(item=> {
+		item.classList.remove('active');
+	})
+	form.classList.add('active');
+}
+
+
+const registerPassword = document.querySelector('form.register #password');
+const registerConfirmPassword = document.querySelector('form.register #confirm-pass');
+
+registerPassword.addEventListener('input', function () {
+	registerConfirmPassword.pattern = `${this.value}`;
+})
