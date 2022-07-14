f82a7682bdb242a1aad2d2ec6d2f346be992f39f
Dinesh-Talwadker <talwadkerdinesh@gmail.com>
2022-07-14 13:42:19 +0530

    Updated files and ready for heroku hosting

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,11 @@ const path = require("path")
 const app = express()
 
 // style files loaded
-app.use(express.static(path.join(__dirname)));
-app.use(express.static('../'));
+app.use(express.static(path.join(__dirname, '../')));
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
 
 // routes 
-
 const MainRouter = require("../backend/routes/main")
 const UserRouter = require("../backend/routes/user")
 
